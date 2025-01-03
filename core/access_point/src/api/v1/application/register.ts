83ad4da2ab43bc3db4f328814a0321810ff6043d
Amrakk <4mr4kk@gmail.com>
2025-01-03 12:20:33 +0700

    feat: implement app registry key

--- core/access_point/src/api/v1/application/register.ts
+++ core/access_point/src/api/v1/application/register.ts
@@ -1,6 +1,8 @@
 import ApiController from "../../apiController.js";
-import { RESPONSE_CODE, RESPONSE_MESSAGE } from "../../../constants.js";
 import ApplicationService from "../../../services/internal/application.js";
+import { APP_REGISTRY_KEY, RESPONSE_CODE, RESPONSE_MESSAGE } from "../../../constants.js";
+
+import ForbiddenError from "../../../errors/ForbiddenError.js";
 
 import type { IReqApplication } from "../../../interfaces/api/request.js";
 import type { IApplication } from "../../../interfaces/database/application.js";
@@ -9,10 +11,13 @@ import type { IApplication } from "../../../interfaces/database/application.js";
 export const register = ApiController.callbackFactory<{}, { body: IReqApplication.Register }, IApplication>(
     async (req, res, next) => {
         try {
-            const { body } = req;
+            const { body, headers } = req;
+            const { "x-app-registry-key": appRegistryKey } = headers;
+
+            if (appRegistryKey !== APP_REGISTRY_KEY) throw new ForbiddenError();
 
             let application = await ApplicationService.getByName(body.name);
-            
+
             if (application) await ApplicationService.updateById(application._id, body);
             else application = (await ApplicationService.insert([body]))[0];
 
--- core/access_point/src/routes/applicationRouter.ts
+++ core/access_point/src/routes/applicationRouter.ts
@@ -3,12 +3,13 @@ import { api } from "../api/index.js";
 
 const applicationRouter = express.Router();
 
-applicationRouter.get("/:id", api.application.getById);
-applicationRouter.patch("/:id", api.application.updateById);
-applicationRouter.delete("/:id", api.application.deleteById);
-
 applicationRouter.get("", api.application.getAll);
+applicationRouter.get("/:id", api.application.getById);
+
 applicationRouter.post("", api.application.insert);
 applicationRouter.post("/register", api.application.register);
+
+applicationRouter.patch("/:id", api.application.updateById);
+applicationRouter.delete("/:id", api.application.deleteById);
 
 export default applicationRouter;
