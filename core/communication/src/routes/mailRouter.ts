f4adcbdc83fe9751180c8a92e6f921732d758f88
Amrakk <4mr4kk@gmail.com>
2025-01-20 00:18:30 +0700

    feat: implement send invitation mail

--- core/communication/src/routes/mailRouter.ts
+++ core/communication/src/routes/mailRouter.ts
@@ -4,5 +4,6 @@ import { api } from "../api/index.js";
 const mailRouter = express.Router();
 
 mailRouter.post("/send-forgot-otp", api.mail.sendForgotOTP);
+mailRouter.post("/send-invitation", api.mail.sendInvitation);
 
 export default mailRouter;
--- core/communication/src/services/internal/mail.ts
+++ core/communication/src/services/internal/mail.ts
@@ -1,7 +1,10 @@
 import fs from "fs";
 import path from "path";
 import { createTransport } from "nodemailer";
+import { formatDate } from "../../utils/formatDate.js";
 import { EMAIL, EMAIL_PASS, APP_NAME } from "../../constants.js";
+
+import type { IReqMail } from "../../interfaces/api/request.js";
 
 export default class MailService {
     public static async sendForgotOTP(email: string, otp: number) {
@@ -26,5 +29,39 @@ export default class MailService {
         };
 
         await transporter.sendMail(mailOptions);
+    }
+
+    public static async sendInvitation(data: IReqMail.SendInvitation) {
+        const transporter = createTransport({
+            service: "gmail",
+            auth: {
+                user: EMAIL,
+                pass: EMAIL_PASS,
+            },
+        });
+
+        const absPath = path.join(process.cwd(), "templates", "groupInvitation.html");
+        const template = await fs.promises.readFile(absPath, "utf8");
+
+        for (const recipient of data.recipients) {
+            let html = template;
+
+            html = html.replaceAll("{{ appName }}", APP_NAME);
+            html = html.replace("{{ recipientName }}", recipient.name);
+            html = html.replace("{{ senderName }}", data.senderName);
+            html = html.replace("{{ groupName }}", data.groupName);
+            html = html.replace("{{ role }}", recipient.role);
+            html = html.replace("{{ expiredAt }}", formatDate(recipient.expiredAt));
+            html = html.replace("{{ navigateUrl }}", recipient.navigateUrl);
+
+            const mailOptions = {
+                from: `${APP_NAME} <${EMAIL}>`,
+                to: recipient.email,
+                subject: `Invitation to Join ${data.groupName}`,
+                html,
+            };
+
+            transporter.sendMail(mailOptions);
+        }
     }
 }
